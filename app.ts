d715f53c69ed9942e5b50b3d48cf63dd34c87505
icaazual <o.arth@live.de>
2020-10-10 23:13:13 +0200

    gfdf

--- app.ts
+++ app.ts
@@ -1,19 +1,52 @@
-import './setup'
+import { get2dContext } from './setup'
 
-export class Hero {
-  id: number
-  name: string
-
-  constructor(name: string) {
-    this.name = name
-    this.id = 1
-  }
-
-  myName() {
-    return this.name
+class Point {
+  x: number
+  y: number
+  constructor(x: number, y: number) {
+    this.x = x
+    this.y = y
   }
 }
 
-debugger
-let hero = new Hero('krunal')
-console.log(hero.myName())
+const ctx = get2dContext()
+
+setInterval(tick, 15 / 1000)
+
+const sizeX = 300
+const sizeY = 150
+const boxSize = 10
+
+let x = 0
+let y = 0
+
+let xv = 1
+let yv = 1
+
+let tail: Point[] = []
+let tailLength = 20
+
+function tick() {
+  ctx.fillStyle = 'black'
+  ctx.fillRect(0, 0, sizeX, sizeY)
+
+  ctx.fillStyle = 'red'
+  ctx.fillRect(x, y, 10, 10)
+
+  if (x + boxSize + xv > sizeX || x + xv < 0) xv = -xv
+  if (y + boxSize + yv > sizeY || y + yv < 0) yv = -yv
+
+  tail.push(new Point(x, y))
+
+  while (tail.length > tailLength) {
+    tail.shift()
+  }
+
+  for (let i = 0; i < tail.length; i++) {
+    ctx.fillStyle = 'red'
+    ctx.fillRect(tail[i].x, tail[i].y, 10, 10)
+  }
+
+  x = x + xv
+  y = y + yv
+}
--- setup.ts
+++ setup.ts
@@ -1,21 +1,16 @@
-function setup() {
+export function get2dContext(): CanvasRenderingContext2D {
   let canvas = getCanvas()
   let ctx = getContext(canvas)
-
-  ctx.fillStyle = 'black'
-  ctx.fillRect(0, 0, 500, 500)
-  ctx.fill()
-
-  function getCanvas(): HTMLCanvasElement {
-    let canvasElement: HTMLElement | null = document.getElementById('canvas')
-    if (canvasElement === null) throw 'Unable to get canvas element'
-    return canvasElement as HTMLCanvasElement
-  }
-  function getContext(canvas: HTMLCanvasElement): CanvasRenderingContext2D {
-    let ctx: CanvasRenderingContext2D | null = canvas.getContext('2d')
-    if (ctx === null) throw 'Unable to get context'
-    return ctx
-  }
+  return ctx
 }
 
-setup()
+function getCanvas(): HTMLCanvasElement {
+  let canvasElement: HTMLElement | null = document.getElementById('canvas')
+  if (canvasElement === null) throw 'Unable to get canvas element'
+  return canvasElement as HTMLCanvasElement
+}
+function getContext(canvas: HTMLCanvasElement): CanvasRenderingContext2D {
+  let ctx: CanvasRenderingContext2D | null = canvas.getContext('2d')
+  if (ctx === null) throw 'Unable to get context'
+  return ctx
+}

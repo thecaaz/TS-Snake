39dc4cad2397aaf44fb904207048b49e88ae83a4
thecaaz <o.arth@live.de>
2020-10-13 01:20:43 +0200

    * remove 2 player test

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,15 @@
-import { get2dContext } from './setup'
 import { Direction } from './Classes/Enums'
-import Point from './Classes/Point'
 import Apple from './Classes/Apple'
 import Settings from './settings'
 import Player from './Classes/Player'
 import IDrawable from './Interfaces/IDrawable'
 import IGameObject from './Interfaces/IGameObject'
 
-debugger
 const player = new Player()
-const player2 = new Player()
 let apple = new Apple(player)
 
-const drawables: IDrawable[] = [player, apple, player2]
-const gameObjects: IGameObject[] = [player, player2, apple]
+const drawables: IDrawable[] = [player, apple]
+const gameObjects: IGameObject[] = [player, apple]
 
 setInterval(gameTick, 1000 / Settings.fps)
 
